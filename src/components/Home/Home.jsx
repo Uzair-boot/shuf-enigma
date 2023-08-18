58262782b880efd6b435a6d75bc3d6ce6e83951f
uzair-boot <ozairahmad984@gmail.com>
2023-08-18 12:55:44 +0500

    changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ import Technologies from "./Technologies";
 import ScrollArrow from "../ScrollArrow/ScrollArrow";
 import Footer from "../Footer/Footer";
 import { NavLink } from "react-router-dom";
-import background from "../../assets/Home/bg.png";
+import background from "../../assets/Home/bg.jpg";
 
 export default function Home() {
   const largeScreen = useMediaQuery("(min-width: 1200px)");
@@ -30,59 +30,54 @@ export default function Home() {
                 textAlign: "center",
               }}
             >
-              
-                <Box>
-                  <HeaderTypography
-                    myfontsize={myfontsize}
-                    color="#114D94"
-                    lineHeight="normal"
+              <Box>
+                <HeaderTypography
+                  myfontsize={myfontsize}
+                  color="#116D6E !important"
+                  lineHeight="normal"
+                >
+                  Empowering Innovation
+                </HeaderTypography>
+                <HeaderTypography
+                  myfontsize={myfontsize}
+                  color="#4E3636 !important "
+                >
+                  Through Seamless Software Solutions
+                </HeaderTypography>
+                <Box px={4}>
+                  <GenerelTypography
+                    sx={{
+                      textAlign: largeScreen
+                        ? "justify !important"
+                        : "center !important",
+                    }}
                   >
-                    WE TRANSFORM BUSINESSES WITH
-                  </HeaderTypography>
-                  <HeaderTypography
-                    myfontsize={myfontsize}
-                    color="#79B5D9 !important"
-                  >
-                    TECH SOLUTIONS
-                  </HeaderTypography>
-                  <Box px={4}>
-                    <GenerelTypography
-                      sx={{
-                        textAlign: largeScreen
-                          ? "justify !important"
-                          : "center !important",
-                      }}
-                    >
-                      Our mission is to enhance the business growth of our
-                      customers with creative designs, development and to
-                      deliver market-defining high-quality solutions that create
-                      value and reliable competitive advantage of customers
-                      around the globe.
-                    </GenerelTypography>
-                  </Box>
-                  <AboutButton>
-                    <NavLink
-                      to="/contactus"
-                      style={{ color: "#ffffff", textDecoration: "none" }}
-                    >
-                      Contact Us
-                    </NavLink>
-                  </AboutButton>
+                    We are a leading software company specializing in innovative
+                    solutions for businesses. With a customer-centric approach
+                    and cutting-edge technology, we help organizations embrace
+                    digital transformation to achieve their goals.
+                  </GenerelTypography>
                 </Box>
-              
+                <AboutButton>
+                  <NavLink
+                    to="/contactus"
+                    style={{ color: "#ffffff", textDecoration: "none" }}
+                  >
+                    Contact Us
+                  </NavLink>
+                </AboutButton>
+              </Box>
             </Grid>
             <Grid item xs={12} md={12} lg={6}>
-              
-                <Box sx={{ display: { xs: "none", lg: "block" } }}>
-                  <img
-                    style={{
-                      width: "100%",
-                    }}
-                    src={background}
-                    alt=""
-                  />
-                </Box>
-              
+              <Box sx={{ display: { xs: "none", lg: "block" } }}>
+                <img
+                  style={{
+                    width: "100%",
+                  }}
+                  src={background}
+                  alt=""
+                />
+              </Box>
             </Grid>
           </Grid>
 
